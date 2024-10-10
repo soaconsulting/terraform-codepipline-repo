de52dd78726cd9c77fa3aa251f2d99d7d66ccca0
Jimmy Vélez Muñoz <jimmyvelezmunoz@gmail.com>
2024-10-10 17:09:55 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "key_name" {
 
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket for remote state"
-  default = "jvelez-terraform-codepipline-bucket"
+  default = "jvelez-terraform-codepipeline-bucket"
 }
 
 variable "name" {
